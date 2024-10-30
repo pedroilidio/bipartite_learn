6de90cc1dccee0f0f465c151843f63b5959eebe2
Pedro Ilídio <pedro.ilidio@kuleuven.be>
2024-10-30 15:45:49 +0100

    fix numpy version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     install_requires=[
         'cython==0.29.33',
         'scikit-learn==1.3.0',
-        'numpy>=1.22.2',
+        'numpy==1.26.4',
         'imbalanced-learn==0.9.1',
     ],
     extras_require={
